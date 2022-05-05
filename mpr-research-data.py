5d825453d1bf41394a6192dd6c3d772e06ba68d2
Kapotaksha Das <kapotakshadas@gmail.com>
2022-05-05 09:21:05 -0400

    Made script Docker compatible

--- mpr-research-data.py
+++ mpr-research-data.py
@@ -1,16 +1,14 @@
 import json
-import sys
-
 import pandas as pd
 import sqlalchemy as sql
+import os
+import sys
 
 dbParams = json.load(open('database-research_ro.json'))
 connectString = f'mysql+pymysql://{dbParams["USER"]}:{dbParams["PASSWORD"]}@{dbParams["HOST"]}:{dbParams["PORT"]}/{dbParams["NAME"]}'
 engine = sql.create_engine(connectString)
 
-# appropriate number of months to approximate the length of academic term
-# by trial and error, 4 seems to be good
-# FIXME: make this a configuration parameter
+
 numberOfMonths = 4
 
 courseQuery = f'''
@@ -28,24 +26,60 @@ ORDER BY
   ca.due_date_utc DESC
 '''
 
+
+def courseFilter(courseDF):
+    courseIDList = []
+
+    for index,row in courseDF.iterrows():
+        if '22' in row['name']:
+            courseIDList.append(row['id'])
+
+    return courseIDList
+    
+def queryMaker(courseIDList, templateQuery = 'testQuery.sql'):
+    with open(templateQuery) as queryFile:
+        queryLines = ''.join(queryFile.readlines())
+
+    courseIDString = ',\n   '.join(map(str, courseIDList))+'\n'
+    newQuery = queryLines.format(courseIDString)
+
+    return newQuery
+
+def queryBulkPull(query, engine):
+  with engine.connect() as connection:   
+      reviewDF = pd.read_sql(query, connection)
+
+  return reviewDF
+
+
+def saveQueries(courseDF, reviewDF, savePath='TSVs/'):
+
+  if not os.path.exists(savePath):
+    os.makedirs(savePath)
+
+  for _, row in courseDF.iterrows():
+      (courseID, courseName) = row
+      outputFilename = f'{courseID} - {courseName}.tsv'
+
+      saveDF = reviewDF[reviewDF['CourseID'] == courseID]
+      saveDF.to_csv(savePath+outputFilename, sep="\t", quoting=3, quotechar="", escapechar="\\")
+
+
 with engine.connect() as connection:
     courseDF = pd.read_sql(courseQuery, connection)
+print('Courses retrieved...')
 
-with open('query.sql') as queryFile:
-    reviewQuery = ''.join(queryFile.readlines())
+newQuery = queryMaker(courseDF['id'])
+print('Query built...')
 
-for _, row in courseDF.iterrows():
-    (courseID, courseName) = row
-    outputFilename = f'{courseID} - {courseName}.tsv'
-    print(outputFilename)
+sys.stdout.flush()
 
-    with engine.connect() as connection:
-        reviewDF = pd.read_sql(reviewQuery.format(courseID), connection)
+print('Retrieving...')
+reviewDF = queryBulkPull(newQuery, engine)
 
-    print(reviewDF)
+sys.stdout.flush()
 
-    sys.stdout.flush()
+print('Saving...')
+saveQueries(courseDF, reviewDF)
 
-    # FIXME: format reviewDF as text file and store in GCP
-    # Preferrably, format data as TSV (CSV only if *necessary*).
-    # Upload data to GCP and store in bucket.
+print('All done!')
